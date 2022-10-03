39df79b1766dc83be0a01d12f8d1e7c865105e2e
= <=>
2022-10-02 21:05:05 -0500

    Primera version

---
+++ baseCamisetas.sql
@@ -0,0 +1,53 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.9.2-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando datos para la tabla tiendacamisetas.carrito: ~0 rows (aproximadamente)
+DELETE FROM `carrito`;
+/*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
+
+-- Volcando datos para la tabla tiendacamisetas.categorias: ~0 rows (aproximadamente)
+DELETE FROM `categorias`;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+
+-- Volcando datos para la tabla tiendacamisetas.detallepagos: ~0 rows (aproximadamente)
+DELETE FROM `detallepagos`;
+/*!40000 ALTER TABLE `detallepagos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detallepagos` ENABLE KEYS */;
+
+-- Volcando datos para la tabla tiendacamisetas.pagos: ~0 rows (aproximadamente)
+DELETE FROM `pagos`;
+/*!40000 ALTER TABLE `pagos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagos` ENABLE KEYS */;
+
+-- Volcando datos para la tabla tiendacamisetas.productos: ~0 rows (aproximadamente)
+DELETE FROM `productos`;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+
+-- Volcando datos para la tabla tiendacamisetas.tipopersona: ~0 rows (aproximadamente)
+DELETE FROM `tipopersona`;
+/*!40000 ALTER TABLE `tipopersona` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipopersona` ENABLE KEYS */;
+
+-- Volcando datos para la tabla tiendacamisetas.usuarios: ~0 rows (aproximadamente)
+DELETE FROM `usuarios`;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
